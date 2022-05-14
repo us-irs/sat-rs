21c53c07ab1c0014a6cf5701311aebace066c0f7
Robin Mueller <robin.mueller.m@gmail.com>
2022-05-14 21:53:59 +0200

    eeeh

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fmt;
+use std::error::Error;
+use thiserror::Error;
+
+struct Event {
+    event_id: u32
+}
+
+trait SystemObject {
+    fn get_object_id(&self) -> u32;
+    fn initialize(&mut self) -> Result<(), Box<dyn Error>>;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World");
 }
