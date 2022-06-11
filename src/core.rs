6bc87746c68edc98ef8a5997e2461ee9861c3b6d
Robin Mueller <robin.mueller.m@gmail.com>
2022-06-11 12:37:54 +0200

    typo

--- src/core.rs
+++ src/core.rs
@@ -3,7 +3,7 @@
 //! This includes components to perform the following tasks
 //!
 //! 1. Object Management with the [objects] module
-//! 2. Task schedule with the [executable] module
+//! 2. Task scheduling with the [executable] module
 //! 3. Events with the [events] module and event management with the [event_man] module
 //! 4. Pre-Allocated memory pools with the [pool] module
 pub mod event_man;
