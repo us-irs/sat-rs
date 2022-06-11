ac6b9fc99c86aea64eba299a81a1964cc7d59516
Robin Mueller <robin.mueller.m@gmail.com>
2022-06-11 12:30:20 +0200

    improved docs

--- src/core.rs
+++ src/core.rs
@@ -1,10 +1,11 @@
-//! This module contains the core components of the Flight Software Rust Crate (FSRC) collection
+//! # Core components of the Flight Software Rust Crate (FSRC) collection.
+//!
 //! This includes components to perform the following tasks
 //!
-//! 1. [Object Management][objects]
-//! 2. [Task Scheduling][executable]
-//! 3. [Events][event] and [event management][event_man]
-//! 4. [Pre-Allocated memory pools][pool]
+//! 1. Object Management with the [objects] module
+//! 2. Task schedule with the [executable] module
+//! 3. Events with the [events] module and event management with the [event_man] module
+//! 4. Pre-Allocated memory pools with the [pool] module
 pub mod event_man;
 pub mod events;
 pub mod executable;
