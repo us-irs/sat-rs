f24b27ccaf165dae74432d397df9356f084c1c49
Robin Mueller <robin.mueller.m@gmail.com>
2023-01-11 09:49:34 +0100

    remove satrs-core to replace with submodule

--- satrs-core/src/pus/hk.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum Subservice {
-    TcEnableGeneration = 5,
-    TcDisableGeneration = 6,
-    TmHkPacket = 25,
-    TcGenerateOneShotHk = 27,
-    TcModifyCollectionInterval = 31,
-}
