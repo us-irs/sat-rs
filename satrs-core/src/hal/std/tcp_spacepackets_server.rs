54bc37b0861252e1262cc27b4eb70a1f5a5b2753
Robin Mueller <robin.mueller.m@gmail.com>
2023-09-18 14:57:27 +0200

    fix clippy

--- satrs-core/src/hal/std/tcp_spacepackets_server.rs
+++ satrs-core/src/hal/std/tcp_spacepackets_server.rs
@@ -15,18 +15,14 @@ pub trait ApidLookup {
 /// The parser will write all packets which were decoded successfully to the given `tc_receiver`.
 pub fn parse_buffer_for_ccsds_space_packets<E>(
     buf: &mut [u8],
-    apid_lookup: &dyn ApidLookup,
-    tc_receiver: &mut dyn ReceivesTc<Error = E>,
-    next_write_idx: &mut usize,
+    _apid_lookup: &dyn ApidLookup,
+    _tc_receiver: &mut dyn ReceivesTc<Error = E>,
+    _next_write_idx: &mut usize,
 ) -> Result<u32, E> {
-    let mut start_index_packet = 0;
-    let mut start_found = false;
-    let mut last_byte = false;
-    let mut packets_found = 0;
-    for i in 0..buf.len() {
+    let packets_found = 0;
+    for _ in 0..buf.len() {
         todo!();
     }
     // Split frame at the end for a multi-packet frame. Move it to the front of the buffer.
-    if start_index_packet > 0 && start_found && packets_found > 0 {}
     Ok(packets_found)
 }
